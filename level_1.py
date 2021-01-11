8cd1d6bf49d6c36f81efff2c2e1306b54aa4eecb
agioio <agioio@student.wethinkcode.co.za>
2021-01-11 12:29:27 +0200

    Level 1 completed

---
+++ level_1.py
@@ -0,0 +1,7 @@
+from string import ascii_lowercase as al
+
+text = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""
+
+table = text.maketrans(al,al[2:] + al[:2])
+
+print(text.translate(table))
